288010b0803ad7f2095161130fe394693bf0ad3c
Rowe <130283000+RoweRui@users.noreply.github.com>
2024-10-06 08:18:00 +0800

    Add files via upload

---
+++ src/data/home.ts
@@ -0,0 +1,462 @@
+export const categories = [
+  "All",
+  "Programming",
+  "Animations",
+  "Trailer",
+  "React",
+  "Top Movies",
+  "Music",
+  "Web Development",
+  "Coding",
+]
+
+export const videos = [
+  {
+    id: "1",
+    title: "Lauv - Steal The Show",
+    channel: {
+      name: "DisneyMusicVevo",
+      id: "Disney",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AIdro_mUg1k_mINBOyGoW6fXKTVg0E4aTcelisOTIhioxdFD6oI=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 1460018,
+    postedAt: new Date("2023-08-29"),
+    duration: 938,
+    thumbnailUrl: "https://i.ytimg.com/vi/DwuJeGYlYyw/hq720.jpg?sqp=-oaymwEpCNAFEJQDSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIHBACGAYgATgBQAE=&rs=AOn4CLDkWIPSqszQU7RNE-41aLzrHeet0A",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "2",
+    title: "Taylor Swift - Fortnight (feat. Post Malone) (Official Music Video)",
+    channel: {
+      name: "Taylor Swift",
+      id: "TaylorSwift",
+      profileUrl:
+        "https://yt3.googleusercontent.com/wp-YrJ2bHsq4jKC1CLqzMRmaUhb8Iuk1V1REPTBUwxhchHpPrHCIO27bhQrzFzusHLRarepJQg=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+    views: 257136,
+    postedAt: new Date("2023-08-22"),
+    duration: 732,
+    thumbnailUrl: "https://i.ytimg.com/vi/q3zqJs7JUCQ/hq720.jpg?sqp=-oaymwEpCNAFEJQDSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIHBACGAYgATgBQAE=&rs=AOn4CLDVMgciyH0djnLWlCYdSpD-FcsAtA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "3",
+    title: "Taylor Swift - Cruel Summer (Official Audio)",
+    channel: {
+      name: "Taylor Swift",
+      id: "TaylorSwift",
+      profileUrl:
+        "https://yt3.googleusercontent.com/wp-YrJ2bHsq4jKC1CLqzMRmaUhb8Iuk1V1REPTBUwxhchHpPrHCIO27bhQrzFzusHLRarepJQg=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+    views: 1232300,
+    postedAt: new Date("2023-10-05"),
+    duration: 120,
+    thumbnailUrl: "https://i.ytimg.com/vi/ic8j13piAhQ/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4OgAK4CIoCDAgAEAEYZSBTKGUwDw==&rs=AOn4CLBBMq5LijoYOGcRPVpm1H4RaBEV2w",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "4",
+    title: "The 1975 - About You (Official)",
+    channel: {
+      name: "The1975",
+      id: "The1975",
+      profileUrl:
+        "https://yt3.googleusercontent.com/9OsqoGmOaLWJ9Ke7NAqY-b5HGOh1g6AvjD0qNqPAIOaop6djqn9QLUdwOMHzf8hA5Q7qT5k9Mw=s160-c-k-c0x00ffffff-no-rj",
+    },
+    views: 112,
+    postedAt: new Date("2023-9-19"),
+    duration: 4343,
+    thumbnailUrl: "https://i.ytimg.com/vi/tGv7CUutzqU/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCP7I8BfnSyCVh8a1bmUkP4A9w_5A",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "5",
+    title: "This Is Halloween (From Tim Burton's: The Nightmare Before Christmas)",
+    channel: {
+      name: "Disney Music Vevo",
+      id: "DisneyMusicVevo",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AIdro_mUg1k_mINBOyGoW6fXKTVg0E4aTcelisOTIhioxdFD6oI=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 42345,
+    postedAt: new Date("2023-03-02"),
+    duration: 1000,
+    thumbnailUrl: "https://i.ytimg.com/vi/ZVuToMilP0A/hqdefault.jpg?sqp=-oaymwEpCNACELwBSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIHBACGAYgATgBQAE=&rs=AOn4CLDItEPyDXuYAMXpE8fL1yy-r3sacw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "6",
+    title: "I Got Laid Off...",
+    channel: {
+      name: "Caleb Curry",
+      id: "CalebCurry",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKbpSojje_-tkBQecNtFuPdSCrg3ZT0FhaYjln9k0g=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 10340,
+    postedAt: new Date("2023-03-03"),
+    duration: 54,
+    thumbnailUrl: "https://i.ytimg.com/vi/i2JVQdLnkAY/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "7",
+    title: "Tails OS in 100 Seconds",
+    channel: {
+      name: "Fireship",
+      id: "Fireship",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 10323340,
+    postedAt: new Date("2023-08-09"),
+    duration: 100,
+    thumbnailUrl: "https://i.ytimg.com/vi/mVKAyw0xqxw/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "8",
+    title: "Maroon 5 - Love Somebody",
+    channel: {
+      name: "Maroon 5",
+      id: "Maroon5",
+      profileUrl:
+        "https://yt3.ggpht.com/S_jTelpAHOdADumYkZAtwyLydEkqtv39s3T-Rmif0v1WCDb8pZit-Vlo43pR1jBEn0Tmcpkx=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/MU8B4XDI3Uw/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYViBfKGUwDw==&rs=AOn4CLA1rsHx9g6ib3ub_Z4lro1SWnwVyw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "9",
+    title: "Maki performs 'Dilaw' LIVE on Wish 107.5 Bus",
+    channel: {
+      name: "Wish 107.5 Bus",
+      id: "Wish107.5",
+      profileUrl:
+      "https://yt3.ggpht.com/ytc/AIdro_nYAmpghLDy1OBL5KrIeKKfSofulg1rCM4dRH3NJXEkAvQ=s68-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/Q4LBdR-tQHc/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYfyBDKCQwDw==&rs=AOn4CLCRJv1wOO4v0aH9r6dkaU89pia_lA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "10",
+    title: "December Avenue feat. Moira Dela Torre - Kung 'Di Rin Lang Ikaw (OFFICIAL MUSIC VIDEO)",
+    channel: {
+      name: "TOWERofDOOM",
+      id: "TOWERofDOOM",
+      profileUrl:
+      "https://yt3.ggpht.com/ytc/AIdro_mcvT0ImzShrnXpt0gU2P-7duscK9e_tKA8VdS0VmVmZw=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 1780093,
+    postedAt: new Date("2018-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/P1pwbnzbe7g/hqdefault.jpg?sqp=-oaymwEoCKgBEF5IWvKriqkDGwgBFQAAiEIYAdgBAeIBDAgcEAIYBiABOAFAAQ==&rs=AOn4CLAMsJJyUhYsyO_8ezFFPoqj8pZN0w",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "11",
+    title: "Zig in 100 Seconds",
+    channel: {
+      name: "Fireship",
+      id: "Fireship",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "12",
+    title: "In The Night - Fly By Midnight (Official Lyric Video)",
+    channel: {
+      name: "Fly By Midnight",
+      id: "FlyByMidnight",
+      profileUrl:
+      "https://yt3.ggpht.com/cdkqs-PNlEXB5_eBlkgpz3oJ4t8WIVTLjlE7KWCeHjfcmeM8YAaW9kBFrjXpZwljr9LrDz5tlQ=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/_eXFFOTGD1I/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYNSBRKHIwDw==&rs=AOn4CLAX2oASaEjnuEIdrBlG8xcfPjFM8w",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "13",
+    title: "PEENOISE PLAYS CHAINED TOGETHER",
+    channel: {
+      name: "PeenoisePlays",
+      id: "PeenoisePlays",
+      profileUrl:
+        "https://yt3.ggpht.com/vQ3lCfPafHV2Gzzrq41qlmw1QgXnbX1OWrc0GYu_HBf7Kp6-q3r_IKYPL4DO1r6DoMyM0OG3=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/an_webp/DGWoWHZheIA/mqdefault_6s.webp?du=3000&sqp=CMD6hrgG&rs=AOn4CLCbPJw0poGMuekt5LUwzlYhfAkQhA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "14",
+    title: "SPECIAL TRAILER: #ResbakNiTanggol | FPJ's Batang Quiapo",
+    channel: {
+      name: "ABS CBN",
+      id: "ABSCBN",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AIdro_n4nkRNmqPtbSxfPgRarpJdZbg3zCyAx9qKyZwHUW9Czc8=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/an_webp/g90nvWM_42M/mqdefault_6s.webp?du=3000&sqp=CIHphrgG&rs=AOn4CLCqXXygLH38FMjq_E2cenXuPKsjiA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "15",
+    title: "The HORROR Begins - Resident Evil 8 VILLAGE",
+    channel: {
+      name: "GLOCO",
+      id: "GLOCO",
+      profileUrl:
+        "https://yt3.ggpht.com/Pfzb0IPiztLilF50j-vlxKA6zd9J_IQVVMk7dSAoF-IeyPWtcxeWIa74BkHNxgeB9HsK03Ft7A=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/zr3BYZoN9Zw/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdwjN6uWKWEnthJ_HV7_922I3oCw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "16",
+    title: "Maroon 5 - Payphone ft. Wiz Khalifa (Explicit) (Official Music Video)",
+    channel: {
+      name: "Maroon5",
+      id: "Maroon5",
+      profileUrl:
+        "https://yt3.ggpht.com/S_jTelpAHOdADumYkZAtwyLydEkqtv39s3T-Rmif0v1WCDb8pZit-Vlo43pR1jBEn0Tmcpkx=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/KRaWnd3LJfs/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYZSBcKE8wDw==&rs=AOn4CLBjuNqypMWU_CNA6mmD2sNTqpGDig",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "17",
+    title: "Outside | Official Trailer | Netflix",
+    channel: {
+      name: "Netflix",
+      id: "Netflix",
+      profileUrl:
+        "https://yt3.ggpht.com/dXrF4pqL8wkYSqQ8zgC8iq9vy50k6hH0NQuH46KLR6gbSB3gBIGrus6iqhCn89hc6tksUyEF_yA=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/wN2qR_i7IAg/hq720.jpg?sqp=-oaymwEpCNAFEJQDSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIHBACGAYgATgBQAE=&rs=AOn4CLAJQ7XC1RSnfphVO2fi0IhkrHR-tw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "18",
+    title: "Taylor Swift - Anti-Hero (Official Music Video)",
+    channel: {
+      name: "Taylor Swift",
+      id: "TaylorSwift",
+      profileUrl:
+        "https://yt3.googleusercontent.com/wp-YrJ2bHsq4jKC1CLqzMRmaUhb8Iuk1V1REPTBUwxhchHpPrHCIO27bhQrzFzusHLRarepJQg=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/b1kbLwvqugk/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYZSBiKE0wDw==&rs=AOn4CLBmZ9D0sshumbzBRUHhqUb9GZbgAA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "19",
+    title: "Python vs JavaScript | What to Choose? | ft. Toolify.ai",
+    channel: {
+      name: "CodeWithHarry",
+      id: "CodeWithHarry",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AIdro_m05oPc8I5nhz_ej6JdKoxA6vglaI76AMqtDELBj1s2o0o=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/DAxKQIG4T2Y/hq720.jpg?sqp=-oaymwEpCNAFEJQDSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIHBACGAYgATgBQAE=&rs=AOn4CLCoBWQjFYd73BkSVKx_oA4cyQXT7A",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "20",
+    title: "JavaScript in 2 Minutes",
+    channel: {
+      name: "ProgrammingWithMosh",
+      id: "ProgrammingWithMosh",
+      profileUrl:
+        "https://yt3.ggpht.com/lCeCb47hCbXWFa0I4gi8uWDHzWSs7sjK4FDmk7lFEUMRNp6QRzIQOkwaKhwv7eNKZacRI2uR=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/btpWt1Cas2I/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCaQk5kWa_nNpsFL0xTV2QGfkUEQw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "21",
+    title: "Making a Game in JavaScript with No Experience",
+    channel: {
+      name: "Goodgis",
+      id: "Goodgis",
+      profileUrl:
+      "https://yt3.ggpht.com/1qMz-rH5iQI4GgCKPevtgYl_VMAfnOYJTDac4_xi8VSF-HPSKfWiiFLMpCAPI17JmWBF0sNDR0I=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/r9I4DuGmJ2Y/hq720.jpg?sqp=-oaymwEpCNAFEJQDSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIHBACGAYgATgBQAE=&rs=AOn4CLCLs3YZ9n2xxi1XLNlIISAEP2L4Ng",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "22",
+    title: "Fan-Favorite Moments: Season 3 😂",
+    channel: {
+      name: "Key&Peele",
+      id: "Key&Peele",
+      profileUrl:
+      "https://yt3.googleusercontent.com/ytc/AIdro_kWanecPuRopibAAKfMfmgB757dK65WD7lXvYHrGnToag=s160-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/an_webp/kH6QJzmLYtw/mqdefault_6s.webp?du=3000&sqp=CJiGh7gG&rs=AOn4CLA9XKjFrefPrdRgsKnxULdpL55QoA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "23",
+    title: "LANY - the older you get, the less you cry (official lyric video)",
+    channel: {
+      name: "LANY",
+      id: "LANY",
+      profileUrl:
+      "https://yt3.ggpht.com/wXlXbNOkJZ0ROapJTZfm7Dehl5Ai1JPjlYQ7XApmyuYio8irXCVLNbr_150T8AXezaJ9h330vw=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/3XWgQr4bVV4/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYciBBKE4wDw==&rs=AOn4CLCYpFvdKBJeSVxAj8DLFHZjf0oLpA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "24",
+    title: "LANY - Thick And Thin (Official Video)",
+    channel: {
+      name: "LANY",
+      id: "LANY",
+      profileUrl:
+      "https://yt3.ggpht.com/wXlXbNOkJZ0ROapJTZfm7Dehl5Ai1JPjlYQ7XApmyuYio8irXCVLNbr_150T8AXezaJ9h330vw=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/ZpUxgAXCK2E/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYfyBGKCswDw==&rs=AOn4CLBLaIIUrJ4ly-ZshawoibWv1w3wMA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "25",
+    title: "Master Software Testing & Automation | Career in Testing | GeeksforGeeks",
+    channel: {
+      name: "GeeksforGeeks",
+      id: "GeeksforGeeks",
+      profileUrl:
+        "https://yt3.googleusercontent.com/tLpsGKu4YqiLMGFmgZ63zgXx2f_k8hot43hUorGKZoWN2FX4Vjh_1znGIhkVQUMM5v0PaJpLBA=s160-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/an_webp/b3uovZLWkVM/mqdefault_6s.webp?du=3000&sqp=CNeTh7gG&rs=AOn4CLCo59zas-aZ7l2aWSE3GZB664x9Nw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "26",
+    title: "Maroon 5 - She Will Be Loved (Official Music Video)",
+    channel: {
+      name: "Maroon5",
+      id: "Maroon5",
+      profileUrl:
+        "https://yt3.ggpht.com/S_jTelpAHOdADumYkZAtwyLydEkqtv39s3T-Rmif0v1WCDb8pZit-Vlo43pR1jBEn0Tmcpkx=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/nIjVuRTm-dc/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYfyBTKBMwDw==&rs=AOn4CLCrWc-mqJ55wemWdvCu4hif89GFhA",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "27",
+    title: "Learn JavaScript by Building 7 Games - Full Course",
+    channel: {
+      name: "freeCodeCamp",
+      id: "freeCodeCamp",
+      profileUrl:
+      "https://yt3.googleusercontent.com/ytc/AIdro_lGRc-05M2OoE1ejQdxeFhyP7OkJg9h4Y-7CK_5je3QqFI=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/an_webp/ec8vSKJuZTk/mqdefault_6s.webp?du=3000&sqp=CJPuhrgG&rs=AOn4CLDt3KvM-zVXhggyaFFAfd_-Hf9NXw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "28",
+    title: "Lauv - All 4 Nothing",
+    channel: {
+      name: "Lauv",
+      id: "Lauv",
+      profileUrl:
+        "https://yt3.ggpht.com/oA8E9QkwrTODpsbG7EmUQHB_AZ6yNJvpyUz-yiUg1NFzf8PD2BlVeR3e47xZmIP7S-aCmaR-=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl: "https://i.ytimg.com/vi/AH8b7EynJ60/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4AbYIgALQBYoCDAgAEAEYWyBBKHIwDw==&rs=AOn4CLBIzGVWVrb_CgKOhh149cmB5Mfbgw",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+]
